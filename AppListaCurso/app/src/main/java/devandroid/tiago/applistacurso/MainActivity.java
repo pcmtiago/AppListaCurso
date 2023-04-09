f6a99c4b419eb4022f4ed00603c90543cc264491
@pcmtiago <pcmtiago@hotmail.com>
2023-04-09 11:33:25 -0300

    APP Lista Primeiro Commit.

--- AppListaCurso/app/src/main/java/devandroid/tiago/applistacurso/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/tiago/applistacurso/MainActivity.java
@@ -3,9 +3,7 @@ package devandroid.tiago.applistacurso;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
