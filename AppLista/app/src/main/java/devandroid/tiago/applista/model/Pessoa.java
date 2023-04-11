7f5cdb84f1d96fa4ef9dc2e06e28a395f396c503
@pcmtiago <pcmtiago@hotmail.com>
2023-04-11 17:15:08 -0300

    novos set no model

--- AppLista/app/src/main/java/devandroid/tiago/applista/model/Pessoa.java
+++ AppLista/app/src/main/java/devandroid/tiago/applista/model/Pessoa.java
@@ -1,8 +1,51 @@
 package devandroid.tiago.applista.model;
 
+//OrdemDePagamento - pascalCase - CamelCase
 public class Pessoa {
 
-    //Contrutor
+    //Construtor
+    //Atributos - Objetos - Molde - Modelo - Template
+    //metodos de Acesso Gets and Sets
+
+    private String primeiroNome;
+    private String sobreNome;
+    private String cursoDesejado;
+    private String telefoneContato;
+
+
+    public String getPrimeiroNome() {
+        return primeiroNome;
+    }
+
+    public void setPrimeiroNome(String primeiroNome) {
+
+        this.primeiroNome = primeiroNome;
+    }
+
+    public String getSobreNome() {
+        return sobreNome;
+    }
+
+    public void setSobreNome(String sobreNome) {
+        this.sobreNome = sobreNome;
+    }
+
+    public String getCursoDesejado() {
+        return cursoDesejado;
+    }
+
+    public void setCursoDesejado(String cursoDesejado) {
+        this.cursoDesejado = cursoDesejado;
+    }
+
+    public String getTelefoneContato() {
+        return telefoneContato;
+    }
+
+    public void setTelefoneContato(String telefoneContato) {
+        this.telefoneContato = telefoneContato;
+    }
+
     public Pessoa(){
 
     }
--- AppLista/app/src/main/java/devandroid/tiago/applista/view/MainActivity.java
+++ AppLista/app/src/main/java/devandroid/tiago/applista/view/MainActivity.java
@@ -11,11 +11,26 @@ import devandroid.tiago.applista.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pessoa = new Pessoa();
+        //Atribuir conteido ao objeto(pessoa)
+        //conforme model
+        pessoa.setPrimeiroNome("Tiago");
+        pessoa.setSobreNome("Oliveira");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("51-992751912");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Luiz");
+        outraPessoa.setSobreNome("Silva");
+        outraPessoa.setCursoDesejado("Android");
+        outraPessoa.setTelefoneContato("51-99999999");
+
+        int parada = 0;
     }
 }
