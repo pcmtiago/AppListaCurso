d221ed9db64745d4b65a5ee810c06f0292f7bb92
@pcmtiago <pcmtiago@hotmail.com>
2023-04-11 18:04:23 -0300

    uso dos metodos get set

--- AppLista/app/src/main/java/devandroid/tiago/applista/view/MainActivity.java
+++ AppLista/app/src/main/java/devandroid/tiago/applista/view/MainActivity.java
@@ -13,12 +13,15 @@ public class MainActivity extends AppCompatActivity {
     Pessoa pessoa;
     Pessoa outraPessoa;
 
+    String dadosPessoa;
+    String dadosOutraPessoa;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         pessoa = new Pessoa();
-        //Atribuir conteido ao objeto(pessoa)
+        //Atribuir contendo ao objeto(pessoa)
         //conforme model
         pessoa.setPrimeiroNome("Tiago");
         pessoa.setSobreNome("Oliveira");
@@ -28,9 +31,30 @@ public class MainActivity extends AppCompatActivity {
         outraPessoa = new Pessoa();
         outraPessoa.setPrimeiroNome("Luiz");
         outraPessoa.setSobreNome("Silva");
-        outraPessoa.setCursoDesejado("Android");
+        outraPessoa.setCursoDesejado("java");
         outraPessoa.setTelefoneContato("51-99999999");
 
-        int parada = 0;
+
+        dadosPessoa = "Primeito nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobreNome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone de Contato: ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeito nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobreNome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone de Contato: ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+        int parada =0;
+
+
     }
 }
